65d21c7dd57f636af961e41ed47b01b5b4003fd8
sonu kaushik <sonukaushik1709@gmail.com>
2025-08-26 21:57:50 +0530

    added

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" rg_sonu1{
+    name = "suraj1"
+    location = "eastus"
+}
+
+resource "azurerm_resource_group" rg_sonu2{
+    name = "suraj2"
+    location = "eastus"
+}
