15bf0fd755b5a97844c44829bd9d1ce08f76a122
sonu kaushik <sonukaushik1709@gmail.com>
2025-08-27 09:44:15 +0530

    Added kamal file and modified main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "azurerm_resource_group" rg_sonu1{
-    name = "suraj1"
+    name = "suraj2"
     location = "eastus"
 }
 
 resource "azurerm_resource_group" rg_sonu2{
-    name = "suraj2"
+    name = "suraj3"
     location = "eastus"
 }
